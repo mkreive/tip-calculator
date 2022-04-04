90965ac97085a1d67c9f1b21a78f902da7c25eeb
mkreive <mkreive@gmail.com>
2022-04-04 15:53:26 +0300

    selectors

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// SELECTORS
+const billElement = document.getElementById("bill");
+const personNumberElement = document.getElementById("persons");
+const tipElement = document.querySelectorAll(".button-tip");
+const customTipElement = document.getElementById("button-custom");
+const tipsCalculatedElement = document.getElementById("tips");
+const totalSumElement = document.getElementById("total");
+const resetButton = document.getElementById("button-reset");
+
+//  CODE
--- style-guide.md
+++
@@ -1,6 +0,0 @@
--   Font size (form inputs): 24px
-
-### Font
-
--   Family: [Space Mono](https://fonts.google.com/specimen/Space+Mono)
--   Weights: 700
